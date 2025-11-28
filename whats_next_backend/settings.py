a71e9b78aacba533d9de96d2592e7be44939f507
Titobioluwa Odulana <emtodulana@gmail.com>
2025-11-28 15:09:12 +0100

    Fixed a few lacking

--- whats_next_backend/settings.py
+++ whats_next_backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-me-in-production')
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,whats-next-oxdf.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
@@ -97,10 +97,14 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://whats-next-oxdf.onrender.com",
+]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://whats-next-oxdf.onrender.com",
 ]
 STATIC_URL = 'static/'
 
